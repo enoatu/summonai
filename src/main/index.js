bf41b31ff7c4f0760e6f6a251a57a80435dfb4c6
enoatu <acro1452@gmail.com>
2023-10-22 00:35:34 +0900

    ブラウザで動作する

--- src/main/index.js
+++ src/main/index.js
@@ -1,101 +1,9 @@
-import { app, globalShortcut, BrowserWindow, ipcMain, Menu, shell, dialog, screen, clipboard } from "electron";
+import { app, BrowserWindow, ipcMain, screen, clipboard } from "electron";
 import { electronApp } from "@electron-toolkit/utils";
 import { uIOhook, UiohookKey } from 'uiohook-napi'
 import robotjs from '@jitsi/robotjs';
 import { sleep, isMac } from "@/utils";
 import { getSelection } from 'node-selection';
-
-// // スウィフトコードを実行
-// const swiftProcess = spawn('swift', [path.join(__dirname, '../..//select_text.swift')]);
-//
-// swiftProcess.stdout.on('data', (data) => {
-//   // Swiftコードからの出力を処理
-//   const selectedText = data.toString();
-//   console.log(`Selected Text: ${selectedText}`);
-// });
-//
-// swiftProcess.stderr.on('data', (data) => {
-//   console.error(`Error: ${data}`);
-// });
-//
-// swiftProcess.on('close', (code) => {
-//   if (code === 0) {
-//     console.log('Swift process finished successfully.');
-//   } else {
-//     console.error(`Swift process exited with code ${code}`);
-//   }
-// });
-
-// app.on("ready", () => {
-//   // グローバルショートカットを設定（例: Ctrl+Shift+C）
-//   globalShortcut.register('Command+Control+C', () => {
-//     // コピー
-//     const script = `
-//     -- アクティブなアプリケーションの名称を取得
-//     tell application "System Events"
-//         keystroke "c" using {command down}
-//     end tell
-//     `
-//     exec(`osascript -e '${script}'`, (error, stdout, stderr) => {
-//       if (error) {
-//         console.error(`Error: ${error.message}`);
-//         return;
-//       }
-//     });
-//
-//     // アクセシビリティAPIを使用して選択しているテキストを取得
-//     const selectedText = clipboard.readText('selection');
-//     console.log('選択したテキスト:', selectedText);
-//
-//   });
-// });
-// 選択したテキストをクリップボードにコピー
-//  const script = `
-//tell application "System Events"
-//   set frontApp to name of first application process whose frontmost is true
-//end tell
-//
-//set targetApps to {"Google Chrome"}
-//set jsScript to "return window.getSelection().toString();"
-//
-//if frontApp is in targetApps then
-//    tell application frontApp
-//        set selectedText to execute javascript jsScript in front window
-//        if selectedText is not "" then
-//            set filePath to "/tmp/selected_text.txt"
-//            do shell script "echo " & quoted form of selectedText & " > " & filePath
-//        end if
-//    end tell
-//else
-//    display dialog "このアプリケーションではサポートされていません。"
-//end if
-//`
-//  exec(`osascript -e '${script}'`, (error, stdout, stderr) => {
-//    if (error) {
-//      console.error(`Error: ${error.message}`);
-//      return;
-//    }
-//  });
-    //
-    // const cursorPosition = screen.getCursorScreenPoint();
-    // const iconSize = { width: 5, height: 2 }; // 2cm * 2cm
-
-    // const icon.window = new BrowserWindow({
-    //   width: iconSize.width,
-    //   height: iconSize.height,
-    //   frame: true, // ウィンドウフレームを非表示に
-    //   transparent: false, // 背景を透明に
-    //   alwaysOnTop: true, // 常に最前面に表示
-    //   webPreferences: {
-    //     nodeIntegration: true,
-    //   },
-    // });
-
-    // icon.window.setPosition(cursorPosition.x, cursorPosition.y);
-    // icon.window.loadFile('src/icon/index.html');
-//   });
-// });
-
 const store = {
   main: {
     isWillQuit: false,
@@ -152,6 +60,7 @@ const store = {
           const { x, y } = screen.getCursorScreenPoint();
           const statusX = winX - x;
           const statusY = winY - y;
+          console.log(x, y, winX, winY)
           if (statusX > 0 && statusX < 20 && statusY > 0 && statusY < 20) {
             console.log("click!!!!!!!!!");
             return;
@@ -252,7 +161,7 @@ const getMacSelectedText = async () => {
     console.log('getSection Success');
   } catch (error) {
     console.log('getSection Failed');
-    releaseKeys();
+    // releaseKeys();
     text = await getSelectedTextByClipboard();
     console.log('getSelectedTextByClipboard Text=', text);
   }
@@ -263,7 +172,7 @@ const getSelectedTextByClipboard = async () => {
   const currentClipboardContent = clipboard.readText();
   console.log("origin clipboard text : ", currentClipboardContent);
   clipboard.clear();
-  await sleep(10)
+  // await sleep(10)
   console.log("do copy");
   robotjs.keyToggle("c", "down", isMac ? "command" : "control");
   await sleep(10)
@@ -276,6 +185,20 @@ const getSelectedTextByClipboard = async () => {
 
 // キーが離される
 const releaseKeys = () => {
+  uIOhook.keyToggle(UiohookKey.Ctrl, "up");
+  uIOhook.keyToggle(UiohookKey.CtrlRight, "up");
+  uIOhook.keyToggle(UiohookKey.Alt, "up");
+  uIOhook.keyToggle(UiohookKey.AltRight, "up");
+  uIOhook.keyToggle(UiohookKey.Shift, "up");
+  uIOhook.keyToggle(UiohookKey.ShiftRight, "up");
+  uIOhook.keyToggle(UiohookKey.Space, "up");
+  uIOhook.keyToggle(UiohookKey.Meta, "up");
+  uIOhook.keyToggle(UiohookKey.MetaRight, "up");
+  uIOhook.keyToggle(UiohookKey.Tab, "up");
+  uIOhook.keyToggle(UiohookKey.Escape, "up");
+}
+
+const releaseKeysForShortSelect = () => {
   uIOhook.keyToggle(UiohookKey.Ctrl, "up");
   uIOhook.keyToggle(UiohookKey.CtrlRight, "up");
   uIOhook.keyToggle(UiohookKey.Alt, "up");
@@ -303,183 +226,7 @@ app.on("ready", async() => {
     }
   }
   start();
-  // const interval = setInterval(async() => {
-  //   console.log(store)
-  //   if (store.main.isWillQuit) {
-  //     clearInterval(interval);
-  //     return;
-  //   }
-  //   try {
-  //     const { text, process } = await getSelection();
-  //     console.log('current selection:', { text, process });
-  //     icon.show();
-  //   } catch (error) {
-  //     console.error('error', error);
-  //   }
-  // }, 1000);
 });
-
-// const createWindow = () => {
-//   const mainWindow = new BrowserWindow({
-//     width: 800,
-//     height: is.dev ? 1000 : 600,
-//     webPreferences: {
-//       nodeIntegration: true,
-//       preload: path.join(__dirname, "../preload/index.js"),
-//       sandbox: false
-//     }
-//   });
-//
-//   const menu = Menu.buildFromTemplate([
-//     {
-//       label: app.name,
-//       submenu: [
-//         {
-//           role: "quit"
-//         }
-//       ]
-//     },
-//     {
-//       role: "windowMenu"
-//     },
-//     {
-//       label: "Information",
-//       submenu: [
-//         {
-//           label: "Help",
-//           click: async () => {
-//             await shell.openExternal(constants.webHelpPageUrl);
-//           }
-//         }
-//       ]
-//     }
-//   ]);
-//   Menu.setApplicationMenu(menu);
-//
-// //   // メインプロセスで `getFilePaths` イベントを受け取ったら、フォルダ内のファイルパスを取得してレンダラープロセスに返す
-// //   ipcMain.handle("selectDir", async (event, path) => {
-// //     const selectedDirs = await dialog.showOpenDialog({
-// //       defaultPath: path || constants.steamPath,
-// //       properties: ["openDirectory"]
-// //     });
-// //     console.log("selectDir", selectedDirs);
-// //     if (!selectedDirs || selectedDirs.cancelled) {
-// //       return "";
-// //     }
-// //     return selectedDirs.filePaths[0];
-// //   });
-// //
-// //   mainWindow.loadFile("index.html");
-// //
-// //  // カスタムUI内のアイコンをクリックしたときのアクション
-// //     mainWindow.webContents.on('did-finish-load', () => {
-// //         mainWindow.webContents.executeJavaScript(`
-// //             const icon = document.getElementById('icon');
-// //             icon.addEventListener('click', () => {
-// //                 const selectedText = window.getSelection().toString();
-// //                 if (selectedText) {
-// //                     clipboard.writeText(selectedText);
-// //                 }
-// //             });
-// //         `);
-// //     });
-//
-// //   const script = `
-// // -- アクティブなアプリケーションの名称を取得
-// // tell application "System Events"
-// //     set frontApp to name of first application process whose frontmost is true
-// // end tell
-// //
-// // -- 対象のアプリケーションのリストを定義
-// // set targetApps to {"Google Chrome", "Safari"} -- 対象のアプリケーションを追加
-// //
-// // -- アクティブなアプリケーションが対象のアプリケーションかを確認
-// // if frontApp is in targetApps then
-// //     -- 選択テキストを取得
-// //     tell application "System Events"
-// //         tell process frontApp
-// //             -- 選択テキストの取得方法をアプリケーションごとに調整
-// //             set selectedText to value of attribute "AXSelectedText" of text area 1 of front window
-// //         end tell
-// //     end tell
-// //
-// //     -- 選択テキストが空でない場合、ファイルに書き込む
-// //     if selectedText is not "" then
-// //         set filePath to "/tmp/selected_text.txt"
-// //         do shell script "echo " & quoted form of selectedText & " > " & filePath
-// //     end if
-// // else
-// //     display dialog "このアプリケーションではサポートされていません。"
-// // end if
-// //   `
-// /*
-// const script = `
-// -- 選択テキストの変更を監視する関数
-// on checkSelectionChange()
-//     tell application "System Events"
-//         set frontApp to name of first application process whose frontmost is true
-//         -- AXSelectedTextの値を取得
-//         set selectedTextValue to (value of attribute "AXSelectedText" of window 1 of process frontApp)
-//         set currentSelection to (selectedTextValue as string)
-//     end tell
-//
-//     -- 選択テキストが変更されたら何らかのアクションを実行
-//     if currentSelection is not previousSelection then
-//         -- ここに選択テキストが変更されたときのアクションを追加
-//         -- 例: ログに記録する、通知を表示する、ファイルに書き出すなど
-//         do shell script "echo " & quoted form of currentSelection & " > /tmp/selected_text.txt"
-//         set previousSelection to currentSelection
-//     end if
-// end checkSelectionChange
-//
-// -- 初期値の設定
-// set previousSelection to ""
-//
-// -- 選択テキストの変更を監視
-// repeat
-//     checkSelectionChange()
-//     delay 1 -- 監視間隔を調整（必要に応じて変更）
-// end repeat
-// `
-//
-//   exec(`osascript -e '${script}'`, (error, stdout, stderr) => {
-//     if (error) {
-//       console.error(`Error: ${error.message}`);
-//       return;
-//     }
-//   });
-//     */
-//   const filePath = '/tmp/selected_text.txt';
-//   const watcher = fs.watch(filePath, (event, filename) => {
-//     if (event === 'change') {
-//       fs.readFile(filePath, 'utf8', (err, data) => {
-//         if (!err) {
-//           // ファイルが変更されたときの処理
-//           console.log(data);
-//
-//           // ここで選択したテキストに対するアクションを実行できます
-//         }
-//       });
-//     }
-//   });
-//   app.on('before-quit', () => {
-//       watcher.close();
-//   });
-//   // HMR for renderer base on electron-vite cli.
-//   // Load the remote URL for development or the local html file for production.
-//   if (is.dev && process.env["ELECTRON_RENDERER_URL"]) {
-//     mainWindow.loadURL(process.env["ELECTRON_RENDERER_URL"]);
-//   } else {
-//     mainWindow.loadFile(path.join(__dirname, "../renderer/index.html"));
-//   }
-//
-//   // devtoolsを開く
-//   console.log({
-//     env: process.env.NODE_ENV
-//   });
-//   mainWindow.webContents.openDevTools();
-// };
-
 app.whenReady().then(() => {
   // Set app user model id for windows
   electronApp.setAppUserModelId("com.enoatu");
